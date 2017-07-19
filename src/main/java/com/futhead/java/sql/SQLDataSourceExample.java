7137a3b3dc43a6cee3c26ec67459f7144fcdb7a2
futhead <futhead@163.com>
2017-07-19 23:52:27 +0800

    add run parquet schemaMerging example

--- src/main/java/com/futhead/java/sql/SQLDataSourceExample.java
+++ src/main/java/com/futhead/java/sql/SQLDataSourceExample.java
@@ -1,10 +1,17 @@
 package com.futhead.java.sql;
 
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import scala.actors.threadpool.Arrays;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by futhead on 17-7-18.
@@ -15,14 +22,62 @@ public class SQLDataSourceExample {
         SparkSession spark = SparkSession.builder().master("local").appName("SQLDataSourceExample").getOrCreate();
 
         runBasicDataSourceExample(spark);
+        rubBasicParquetExample(spark);
+        runParquetSchemaMergingExample(spark);
 
+        spark.stop();
     }
 
     public static void runBasicDataSourceExample(SparkSession spark) {
         Dataset<Row> userDF = spark.read().load("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/examples/src/main/resources/users.parquet");
+        userDF.select("name", "favorite_color").write().save("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/namesAndFavorColors.parquet");
+
+        Dataset<Row> peopleDF = spark.read().format("json").json("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/examples/src/main/resources/people.json");
+        peopleDF.select("name", "age").write().format("parquet").save("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/namesAndages.parquet");
+
+        Dataset<Row> sqlDF = spark.sql("select * from parquet.`/home/futhead/app/spark-2.0.2-bin-hadoop2.6/examples/src/main/resources/users.parquet`");
     }
 
-    public class Square implements Serializable {
+    public static void rubBasicParquetExample(SparkSession spark) {
+        Dataset<Row> parquetFileDF = spark.read().parquet("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/namesAndages.parquet");
+        parquetFileDF.createOrReplaceTempView("parquetFile");
+        Dataset<Row> namesDF = spark.sql("select name from parquetFile where age between 13 and 19");
+        Dataset<String> namesDS = namesDF.map(new MapFunction<Row, String>() {
+            public String call(Row row) throws Exception {
+                return "Name: " + row.get(0);
+            }
+        }, Encoders.STRING());
+        namesDS.show();
+    }
+
+    public static void runParquetSchemaMergingExample(SparkSession spark) {
+        List<Square> squares = new ArrayList<Square>();
+        for (int value = 1; value <= 5; value++) {
+            Square square = new Square();
+            square.setValue(value);
+            square.setSquare(value * value);
+            squares.add(square);
+        }
+
+        Dataset<Row> squaresDF = spark.createDataFrame(squares, Square.class);
+        squaresDF.write().parquet("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/test_table/key=1");
+
+        List<Cube> cubes = new ArrayList<Cube>();
+        for (int value = 6; value < 10; value++) {
+            Cube cube = new Cube();
+            cube.setValue(value);
+            cube.setCube(value * value * value);
+            cubes.add(cube);
+        }
+
+        Dataset<Row> cubesDF = spark.createDataFrame(cubes, Cube.class);
+        cubesDF.write().parquet("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/test_table/key=2");
+
+        Dataset<Row> mergedDF = spark.read().option("mergeSchema", true).parquet("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/test_table");
+        mergedDF.printSchema();
+    }
+
+    public static class Square implements Serializable {
         private int value;
         private int square;
 
