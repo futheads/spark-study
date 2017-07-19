3229dff9aa5bd5649409ade4242fbaddbdf1421c
futhead <futhead@163.com>
2017-07-19 23:51:31 +0800

    rename java file

--- src/main/java/com/futhead/java/spark/WordCount.java
+++ src/main/java/com/futhead/java/spark/WordCount.java
@@ -9,33 +9,36 @@ import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.SparkSession;
+import scala.concurrent.Future$class;
 
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
-public final class JavaWordCount {
+public final class WordCount {
     private static final Pattern SPACE = Pattern.compile(" ");
 
     public static void main(String[] args) throws Exception {
 
-        /*if (args.length < 1) {
-            System.err.println("Usage: JavaWordCount <file>");
+        if (args.length < 1) {
+            System.err.println("Usage: WordCount <file>");
             System.exit(1);
-        }*/
+        }
 
-        SparkConf sc = new SparkConf().setMaster("local");
+        SparkConf sc = new SparkConf().setMaster(args[0])
+                .setAppName("WordCount");
 
         SparkSession spark = SparkSession
                 .builder()
-                .appName("JavaWordCount").config(sc)
+                .config(sc)
                 .getOrCreate();
 
         JavaRDD<String> lines = spark.read().textFile("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/README.md").javaRDD();
 
         JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-            public Iterator<String> call(String s) {
+            public Iterator<String> call(String s) throws Exception {
                 return Arrays.asList(SPACE.split(s)).iterator();
             }
         });
@@ -46,16 +49,15 @@ public final class JavaWordCount {
             }
         });
 
-        JavaPairRDD<String, Integer> counts = ones.reduceByKey(
-                new Function2<Integer, Integer, Integer>() {
-                    public Integer call(Integer i1, Integer i2) {
-                        return i1 + i2;
-                    }
-                });
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1 + v2;
+            }
+        });
 
         List<Tuple2<String, Integer>> output = counts.collect();
-        for (Tuple2<?, ?> tuple : output) {
-            System.out.println(tuple._1() + ": " + tuple._2());
+        for (Tuple2<?, ?> tuple: output) {
+            System.out.println(tuple._1 + ":" + tuple._2());
         }
         spark.stop();
     }
