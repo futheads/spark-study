12a6a6102cd6e9f2ccae445d3ef359152ed323f2
futhead <futhead@163.com>
2017-07-23 23:35:56 +0800

    User defined untyped aggregation is finished

--- src/main/java/com/futhead/java/sql/UserDefinedUntypedAggregation.java
+++ src/main/java/com/futhead/java/sql/UserDefinedUntypedAggregation.java
@@ -21,14 +21,15 @@ public class UserDefinedUntypedAggregation {
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder().appName("UserDefinedUntypedAggregation").master("local").getOrCreate();
 
-        spark.udf().register("MyVerage", new MyAverage());
+        spark.udf().register("myAverage", new MyAverage());
 
         Dataset<Row> df = spark.read().json("/home/futhead/app/spark-2.0.2-bin-hadoop2.6/examples/src/main/resources/employees.json");
 
-        df.createOrReplaceTempView("employees");;
+        df.createOrReplaceTempView("employees");
         df.show();
 
         Dataset<Row> result = spark.sql("select myAverage(salary) as average_salary from employees");
+        result.show();
 
         spark.stop();
     }
@@ -50,35 +51,40 @@ public class UserDefinedUntypedAggregation {
         }
 
         public StructType inputSchema() {
-            return null;
+            return inputSchema;
         }
 
         public StructType bufferSchema() {
-            return null;
+            return bufferSchema;
         }
 
         public DataType dataType() {
-            return null;
+            return DataTypes.DoubleType;
         }
 
         public boolean deterministic() {
-            return false;
+            return true;
         }
 
         public void initialize(MutableAggregationBuffer buffer) {
-
+            buffer.update(0, 0L);
+            buffer.update(1, 0L);
         }
 
         public void update(MutableAggregationBuffer buffer, Row input) {
-
+            if (!input.isNullAt(0)) {
+                buffer.update(0, buffer.getLong(0) + input.getLong(0));
+                buffer.update(1, buffer.getLong(1) + 1);
+            }
         }
 
         public void merge(MutableAggregationBuffer buffer1, Row buffer2) {
-
+            buffer1.update(0, buffer1.getLong(0) + buffer2.getLong(0));
+            buffer1.update(1, buffer1.getLong(1) + buffer2.getLong(1));
         }
 
         public Object evaluate(Row buffer) {
-            return null;
+            return ((double) buffer.getLong(0) / buffer.getLong(1));
         }
     }
 }
